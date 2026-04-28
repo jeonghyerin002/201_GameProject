b399ff41280dd49938bdfca16a4e6f8a7a56ba6e
jeonghyerin002 <jhr102446@gmail.com>
2026-04-29 06:04:30 +0900

    [Feat] 6강 코드 완료

---
+++ Assets/Scripts/CardGame/CharacterStats.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CharacterStats : MonoBehaviour
+{
+    public string characterName;
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    //UI ¿ä¼Ò
+    public Slider healthBar;
+    public TextMeshProUGUI healthText;
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+    }
+
+    public void Heal(int amount)
+    {
+        currentHealth += amount;
+    }
+
+    void Update()
+    {
+        
+    }
+}
